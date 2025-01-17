c9c6a43e904f508792dd506f5db174d9965b4509
Franz <franzbrunohubertklose@gmail.com>
2025-01-17 14:43:17 +0100

    Idelmechaniken angefangen
    muss nochmal drüber geschaut werden
    
    Buttons mit Grind hinzugefügt

--- Assets/Scripts/Manager/IdelManger.cs
+++ Assets/Scripts/Manager/IdelManger.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class IdelManger : MonoBehaviour
 {
+
+    public static IdelManger Instance { get; private set; }
+
+    private float sugarAutoUpgradeAmout;
+    private float flourAutoUpgradeAmout;
+    private float eggsAutoUpgradeAmout;
+    private float butterAutoUpgradeAmout;
+    private float chocolateAutoUpgradeAmout;
+    private float milkAutoUpgradeAmout;
+
+    private float sugarPowerAmout;
+    private float flourPowerAmout;
+    private float eggsPowerAmout;
+    private float butterPowerAmout;
+    private float chocolatePowerAmout;
+    private float milkPowerAmout;
+
+    private User user;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Instance != null)
+        {
+            Debug.Log("Destroying duplicate WebAPI instance.");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateUserRec();
+    }
+
+    private void UpdateUserRec()
+    {
+        user.sugar += sugarAutoUpgradeAmout;
+        user.flour += flourAutoUpgradeAmout;
+        user.eggs += eggsAutoUpgradeAmout;
+        user.butter += butterAutoUpgradeAmout;
+        user.chocolate += chocolateAutoUpgradeAmout;
+        user.milk += milkAutoUpgradeAmout;
     }
 }
---
+++ Assets/Scripts/Objekte/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+[System.Serializable]
+public class User
+{
+    public string steamid;
+    public float cookies;
+    public float sugar;
+    public float flour;
+    public float eggs;
+    public float butter;
+    public float chocolate;
+    public float milk;
+}
